f2310603c03eee3ee3777e643ad4d5c4e04bc131
HassanAmohamed <hrjdjudurheh2000@gmail.com>
2025-06-02 00:57:26 +0300

    u013

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'api/templates')],
+        'DIRS': [os.path.join(BASE_DIR,'api/templates/api')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
