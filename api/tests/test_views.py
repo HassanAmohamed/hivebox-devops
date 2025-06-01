cdb8d4728bc6dd604649fe0f857377d68f9fd5d9
HassanAmohamed <hrjdjudurheh2000@gmail.com>
2025-06-02 01:02:48 +0300

    u13

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -7,7 +7,11 @@ class ApiTests(TestCase):
     @override_settings(
         HIVEBOX_VERSION='0.1.0',
         OPENSENSEMAP_API='https://api.opensensemap.org',
-        SENSEBOX_IDS=['testbox123']
+        SENSEBOX_IDS=[
+            "679652e79697fc0007248229",
+            "631af55c8aecc5001c3298fd",
+            "67837e108e3d610008017850"
+        ]
     )
     def test_version_endpoint(self):
         """Test version endpoint returns correct version"""
@@ -18,7 +22,7 @@ class ApiTests(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['version'], '0.1.0')
-        
+
         # Test HTML response
         response = self.client.get(reverse('version'))
         self.assertEqual(response.status_code, 200)
@@ -26,14 +30,18 @@ class ApiTests(TestCase):
 
     @override_settings(
         OPENSENSEMAP_API='https://api.opensensemap.org',
-        SENSEBOX_IDS=['testbox123']
+        SENSEBOX_IDS=[
+            "679652e79697fc0007248229",
+            "631af55c8aecc5001c3298fd",
+            "67837e108e3d610008017850"
+        ]
     )
     @patch('requests.get')
     def test_temperature_endpoint_success(self, mock_get):
         """Test successful temperature endpoint"""
         test_box_id = "testbox123"
-        
-        # Setup mock responses
+
+        # Setup mock responses for box and measurements
         mock_box_response = MagicMock()
         mock_box_response.json.return_value = {
             '_id': test_box_id,
@@ -44,19 +52,19 @@ class ApiTests(TestCase):
             }]
         }
         mock_box_response.status_code = 200
-        
+
         mock_measurements_response = MagicMock()
         mock_measurements_response.json.return_value = [{
             'createdAt': '2023-01-01T12:00:00Z',
             'value': '22.5'
         }]
         mock_measurements_response.status_code = 200
-        
+
         mock_get.side_effect = [
             mock_box_response,
             mock_measurements_response
         ]
-        
+
         # Test JSON response
         response = self.client.get(
             reverse('temperature'),
@@ -65,10 +73,10 @@ class ApiTests(TestCase):
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.content)
         self.assertEqual(data['samples'], 1)
-        
+
         # Test HTML response
         response = self.client.get(reverse('temperature'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, '22.5')
 
-    # ... rest of your test cases ...
+    # Additional test cases can be added here
